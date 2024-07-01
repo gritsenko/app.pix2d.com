d1f9da961c46dc6a78878e67e526f83ba868ced7
Igor Gritsenko <igor@gritsenko.biz>
2024-07-02 00:45:33 +0300

    Loader fix

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 ﻿import { dotnet } from './dotnet.js'
+import CommonStuff from './CommonStuff.js'
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
+
+CommonStuff.startLoaderProgressbar();
 
 const { dotnetRuntime, setModuleImports, getConfig } = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
 
-setModuleImports("myLocalStorage", {
+setModuleImports("main.js", {
+    appStarted: () => CommonStuff.onAppStarted(),
     get: (key) => localStorage.getItem(key),
     set: (key, value) => {
         localStorage.setItem(key, value);
@@ -21,22 +25,6 @@ setModuleImports("myLocalStorage", {
 //const config = dotnetRuntime.getConfig();
 const config = getConfig();
 
-
-const progressBar = document.getElementById("progress-value");
-
-let progress = 0;
-let maxProgress = 100;
-const timer = setInterval(() => {
-    const d = (maxProgress - progress) / 10;
-    progress = Math.ceil(progress + d);
-
-    if (progressBar !== null) {
-        progressBar.style.width = progress + "%";
-    }
-    //console.log(progress);
-},
-    100);
-
-
 //await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
 await dotnet.run();
+
